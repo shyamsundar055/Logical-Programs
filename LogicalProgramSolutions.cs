817d8ed23b6c6b0595ccf710b18a27e5cd5b009f
Shyam Sundar <vshyamsundar@live.com>
2023-11-20 17:58:39 +0530

    Add project files.

---
+++ LogicalProgramSolutions.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class LogicalProgramSolutions
+    {
+        /// <summary>
+        /// Sum of integer
+        /// </summary>
+        /// <param name="input"></param>
+        public static void SumOfInteger(int input)
+        {
+            string numStr = input.ToString();
+
+            Console.WriteLine($"SumOfInteger : {numStr}");
+
+            int output = 0;
+
+            foreach (char ch in numStr)
+            {
+                //Option 1
+                output += (ch - '0');
+
+                //Option 2
+                //output += (int)char.GetNumericValue(ch);
+
+                //Option 3
+                //output += int.Parse(ch.ToString());
+
+                //Option 4
+                //output += CharUnicodeInfo.GetDecimalDigitValue(ch);
+            }
+
+            Console.WriteLine($"Output : {output}\n");
+        }
+
+        /// <summary>
+        /// Merge two arrays using generics
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="firstArray"></param>
+        /// <param name="secondArray"></param>
+        public static void MergeTwoArray<T>(T[] firstArray, T[] secondArray)
+        {
+            Console.WriteLine($"MergeTwoArray {typeof(T)} : First Array = {string.Join(" ", firstArray)} , Second Array = {string.Join(" ", secondArray)} ");
+
+            T[] outputArray = new T[firstArray.Length + secondArray.Length];
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                outputArray[i] = firstArray[i];
+            }
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                outputArray[firstArray.Length + i] = secondArray[i];
+            }
+
+            Console.WriteLine($"Output : {string.Join(" ", outputArray)}\n");
+        }
+
+        /// <summary>
+        /// Sort an Integer Array
+        /// </summary>
+        /// <param name="inputArray"></param>
+        public static void SortAnIntegerArray(int[] inputArray, string orderType)
+        {
+            Console.WriteLine($"SortAnArray - Order by {orderType}: {string.Join(" ", inputArray)}");
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+
+                int pickerValue = inputArray[i];
+
+                int newPickerPosition = i;
+                int newPickerValue = inputArray[i];
+
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    //TODO: Try delegate function for asc or desc comparison
+                    if (orderType == "asc" && inputArray[j] < newPickerValue)
+                    {
+                        newPickerValue = inputArray[j];
+                        newPickerPosition = j;
+                    }
+                    else if (orderType == "desc" && inputArray[j] > newPickerValue)
+                    {
+                        newPickerValue = inputArray[j];
+                        newPickerPosition = j;
+                    }
+                }
+
+                inputArray[i] = newPickerValue;
+                inputArray[newPickerPosition] = pickerValue;
+            }
+
+            Console.WriteLine($"Output : {string.Join(" ", inputArray)}\n");
+        }
+
+        /// <summary>
+        /// ReverseInput
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        public static void ReverseInput<T>(T input)
+        {
+            Console.WriteLine($"Reverse {typeof(T)} : {input.ToString()}");
+
+            string inputValue = input.ToString();
+            string output = "";
+
+            for (int i = inputValue.Length - 1; i > -1; i--)
+            {
+                output += inputValue[i];
+            }
+
+            Console.WriteLine($"Output : {output}\n");
+        }
+
+        /// <summary>
+        /// IdentifyDuplicateCharacters
+        /// </summary>
+        /// <param name="input"></param>
+        public static void IdentifyDuplicateCharacters(string input)
+        {
+            Console.WriteLine($"IdentifyDuplicateCharacters with Dictionary : {input}");
+
+            Dictionary<char, int> charDetails = new Dictionary<char, int>();
+
+            foreach (char character in input)
+            {
+                if (charDetails.ContainsKey(character))
+                {
+                    charDetails[character] += 1;
+                }
+                else
+                {
+                    charDetails.Add(character, 1);
+                }
+            }
+
+            Console.WriteLine($"Output :");
+            foreach (var item in charDetails)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} ");
+            }
+        }
+
+        /// <summary>
+        /// IdentifyDuplicateCharactersWithIndex
+        /// </summary>
+        /// <param name="input"></param>
+        public static void IdentifyDuplicateCharactersWithIndex(string input)
+        {
+            Console.WriteLine($"\nIdentifyDuplicateCharacters without Dictionary : {input}");
+
+            var charList = input.ToCharArray();
+
+            Console.WriteLine($"Output :");
+
+            for (int i = 0; i < charList.Length; i++)
+            {
+                if (charList[i] != ' ')
+                {
+                    char character = charList[i];
+                    int count = 1;
+                    string indexPostitions = $"{i},";
+
+                    for (int j = i + 1; j < charList.Length; j++)
+                    {
+                        if (charList[j] != ' ' && char.Equals(charList[i], charList[j]))
+                        {
+                            count++;
+                            charList[j] = ' ';
+                            indexPostitions += $"{j},";
+                        }
+                    }
+
+                    Console.WriteLine($"{character} - count:{count},index positions:{indexPostitions} ");
+                }
+            }
+        }
+
+        /// <summary>
+        /// IsPalindromeNumber
+        /// </summary>
+        /// <param name="input"></param>
+        public static void IsPalindromeNumber(int input)
+        {
+            Console.WriteLine($"\nIsPalindromeNumber : {input}");
+
+            int tempNumber = input;
+
+            int remainder, reverseNumber = 0;
+
+            while (input > 0)
+            {
+                remainder = input % 10;
+                reverseNumber = reverseNumber * 10 + remainder;
+                input /= 10;
+            }
+
+            Console.WriteLine($"Given number is{(tempNumber == reverseNumber ? "" : "not")} a Palindrome");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sum of integer
+            LogicalProgramSolutions.SumOfInteger(1234);
+
+
+            //Merge Two String Arrays
+            string[] firstStrArray = { "a", "b", "c" };
+            string[] secondStrArray = { "d", "e", "f","g","h"};
+            LogicalProgramSolutions.MergeTwoArray(firstStrArray, secondStrArray);
+
+
+            //Merge Two Int Arrays
+            int[] firstIntArray = { 1, 2, 3 };
+            int[] secondIntArray = { 4,5,6,7,8,9,10 };
+            LogicalProgramSolutions.MergeTwoArray(firstIntArray, secondIntArray);
+
+
+
+            //Sort an Int Array ascending
+            int[] inputArray = { 4, 7, 1, 3, 2, 4, 5, 6, 0, 2, 9, 11, 100 };
+            LogicalProgramSolutions.SortAnIntegerArray(inputArray,"asc");
+
+
+
+            //Sort an Int Array ascending 
+            LogicalProgramSolutions.SortAnIntegerArray(inputArray, "desc");
+
+
+
+            //Reverse Int
+            LogicalProgramSolutions.ReverseInput(1234559833644);
+
+
+
+            //Reverse String
+             LogicalProgramSolutions.ReverseInput("Shyam Sundar");
+
+
+
+            //Identify Duplicate characters in a string with No.of occurances (case sensitive not handled) - using dictionary
+            LogicalProgramSolutions.IdentifyDuplicateCharacters("shyam sundar");
+
+
+
+            //Identify Duplicate characters in a string with No.of occurances,index postitions (case sensitive not handled)  - without dictionary
+            LogicalProgramSolutions.IdentifyDuplicateCharactersWithIndex("shyam sundar");
+
+
+
+            //Is Palindrome Number
+            LogicalProgramSolutions.IsPalindromeNumber(1001);
+        }
+
+        
+    }
+}
